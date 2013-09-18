dc143018c5c505373f872c75070dc71fe2f19ca3
kent <kent@openissues.ca>
2013-09-18 22:29:01 +0200

    Remove rake tasks from railties.

--- lib/creative_commons_rails/railtie.rb
+++ lib/creative_commons_rails/railtie.rb
@@ -5,10 +5,5 @@ module CreativeCommonsRails
       ActionView::Base.send :include, CreativeCommonsRails::ActionViewHelpers
       I18n.load_path += Dir.glob( File.dirname(__FILE__) + "/../../config/locales/*.{rb,yml}" )
     end
-
-    # load the rake task to re-retrieve the data on available licenses
-    rake_tasks do
-      load File.join(File.dirname(__FILE__),'tasks/retrieve_data.rake')
-    end
   end
 end
